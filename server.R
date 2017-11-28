3d61f49833602df7e697f66187fe106cf9962ce4
kgori <kevin.c.gori@gmail.com>
2017-11-28 16:20:31 +0000

    Adjustable peak heights

--- server.R
+++ server.R
@@ -21,41 +21,108 @@ vaf <- function(p, cnt, cnh, nt, nh) {
     (nh * (1-p) + nt * p) / (cnt*p + cnh*(1-p))
 }
 
+get_grid <- function(input) {
+    # Process input values
+    major.cn <- input$major
+    minor.cn <- input$minor
+    total.cn <- major.cn + minor.cn
+    host.cn <- as.numeric(input$hostcn)
+
+    grid<-expand.grid(host = 0:host.cn, tumour = 0:total.cn)
+    grid$vaf <- apply(grid, 1, function(r) {
+        vaf(input$purity, total.cn, host.cn, r[["tumour"]], r[["host"]])
+    })
+    
+    if (input$type == "Germline") {
+        grid <- grid[grid$host>0 & grid$host <= host.cn, ]
+    }
+    else if (input$type == "Somatic") {
+        grid <- grid[grid$host==0 & grid$tumour > 0, ]
+    }
+    grid
+}
+
+minor.selected <- 1
+
 shinyServer(function(input, output) {
 
+    observe({
+        minor.selected <<- input$minor
+    })
+    
     output$minor <- renderUI({
 
         # GENERATE U.I. FOR MINOR ALLELE COPY NUMBER STATE
-        numericInput("minor", "Minor allele", min(input$major, input$minor),
+        numericInput("minor", "Minor allele", min(input$major, minor.selected),
                      min = 0, max = input$major, step = 1)
+    })
+    
+    output$peak_heights <- renderUI({
+        req(input$major, input$minor, input$purity)
+        # GENERATE U.I. FOR PEAK HEIGHTS
+        grid <- grid()
+        lapply(1:nrow(grid), function(i) {
+            sliderInput(inputId = paste0("Peak", i),
+                        label = paste("Peak", sprintf("host%d tumour%d vaf=%.2f", grid[i, 1], grid[i, 2], grid[i, 3])),
+                        min = 0, max = 1, value = 0.5, step = 0.01)
+        })
+    })
+    
+    grid <- reactive({
+        get_grid(input)
     })
 
     output$distPlot <- renderPlot({
-
-        # Process input values
-        major.cn <- input$major
-        minor.cn <- input$minor
-        total.cn <- major.cn + minor.cn
-        host.cn <- as.numeric(input$hostcn)
-
-        grid<-expand.grid(host = 0:input$hostcn, tumour = 0:total.cn)
-        grid$vaf <- apply(grid, 1, function(r) {
-            vaf(input$purity, total.cn, host.cn, r[["tumour"]], r[["host"]])
-        })
-
-        if (input$type == "Germline") {
-            grid <- grid[grid$host>0 & grid$host <= host.cn, ]
-        }
-        else {
-            grid <- grid[grid$host==0 & grid$tumour > 0, ]
-        }
-
+        req(input$major, input$minor, input$purity, input$total)
+        grid <- grid()
+    
         nvar <- input$total / nrow(grid)
-        x <- apply(grid, 1, function(r) {
-            rbeta(nvar, rpois(nvar, r[["vaf"]]*input$depth), rpois(nvar, (1-r[["vaf"]])*input$depth))
-        })
+        
+        heights <- vector("numeric", nrow(grid))
+        
+        for (i in 1:nrow(grid)) {
+            heights[i] <- input[[paste0("Peak", i)]]
+        }
+        
+        heights <- heights / sum(heights)
+        grid$heights <- heights
 
+        x <- unlist(apply(grid, 1, function(r) {
+            nv <- round(nvar * r[["heights"]], 0)
+            fnr <- input$fneg
+            fpr <- input$fpos
+            rbeta(nv, rpois(nv, (fpr + r[["vaf"]])*input$depth), rpois(nv, (fnr + 1 - r[["vaf"]])*input$depth))
+        }))
+        
         # draw the histogram with the specified number of bins
-        hist(x, breaks = input$bins, xlim = c(0, 1), freq = FALSE, col = 'darkgray', border = 'white')
+        hist(x, breaks = input$bins, xlim = c(0, 1), freq = TRUE, col = 'darkgray', border = 'white')
+        segments(grid$vaf, 0, grid$vaf, -1, lwd = 4, col = "slateblue3")
+    })
+    
+    output$grid <- renderTable({
+        # x_str <- function(e) {
+        #     if(is.null(e)) return("out of bounds\n")
+        #     paste0(round(e$x, 2), "\n")
+        # }
+        # 
+        # paste0("cursor position=", x_str(input$plot_hover))
+        grid <- grid()
+        heights <- vector("numeric", nrow(grid))
+        
+        for (i in 1:nrow(grid)) {
+            heights[i] <- input[[paste0("Peak", i)]]
+        }
+        
+        heights <- heights / sum(heights)
+        grid$heights <- heights
+        grid
     })
 })
+
+vafhist <- function(nvar, vafs, depth, heights) {
+    stopifnot(length(vafs) == length(heights))
+    unlist(sapply(1:length(vafs), function(i) {
+        nv <- round(nvar*heights[i], 0)
+        rbeta(nv, rpois(nv, vafs[i] * depth), rpois(nv, max(0.001, (1-vafs[i])) * depth))
+    }))
+}
--- ui.R
+++ ui.R
@@ -21,32 +21,42 @@ shinyUI(fluidPage(
                          "Total variants", 50000,
                          min = 0, max = 100000000, step = 10000),
 
-            # HETEROZYGOUS FRACTION
-            # sliderInput("hetfrac",
-            #             "Fraction heterozygous",
-            #             min = 0, max = 1, value = 0.8, step = 0.01, round = 2),
-
+            # READ DEPTH
+            sliderInput("depth",
+                        "Read depth",
+                        min = 10, max = 1000, value = 60),
+            
             # HOST COPY NUMBER
             radioButtons("hostcn",
                         "Host Copy Number",
                         choices = c(1, 2), selected = 2, inline = TRUE),
-
-            # READ DEPTH
-            sliderInput("depth",
-                        "Read depth",
-                        min = 10, max = 200, value = 60),
 
             # SAMPLE PURITY
             sliderInput("purity",
                         "Sample Purity",
                         min = 0, max = 1, value = 0.7, step = 0.01, round = 2),
 
+            # FALSE POSITIVES / FALSE NEGATIVES
+            splitLayout(
+                cellWidths = c("50%", "50%"),
+                cellArgs = list(style = "padding: 6px"),
+                sliderInput("fpos",
+                            "False Positives",
+                            min = 0, max = 0.1, value = 0.01, step = 0.01, round = 2),
+                sliderInput("fneg",
+                            "False Negatives",
+                            min = 0, max = 0.1, value = 0.01, step = 0.01, round = 2)
+            ),
+
             # NUMBER OF BINS IN HISTOGRAM
             sliderInput("bins",
                         "Number of bins:",
                         min = 1,
                         max = 100,
-                        value = 50)
+                        value = 50),
+
+            # PEAK HEIGHTS
+            uiOutput("peak_heights")
         ),
 
         # Show a plot of the generated distribution
@@ -65,13 +75,15 @@ shinyUI(fluidPage(
                 uiOutput("minor"),
 
                 # GERMLINE OR SOMATIC
-                selectizeInput("type", "Variant Type", c("Germline", "Somatic"),
-                               options = list(dropdownParent = 'body'))
+                selectizeInput("type", "Variant Type", c("Germline", "Somatic", "Both"),
+                               options = list(dropdownParent = 'body'))  # So the drop down renders on top of background
             ),
 
             # JUST PLOT HISTOGRAM
-            plotOutput("distPlot")
-
+            plotOutput("distPlot", hover = hoverOpts("plot_hover", delay=10)),
+            
+            # DISPLAY HOVER INFO
+            tableOutput("grid")
         )
     )
 ))
